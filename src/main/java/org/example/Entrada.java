8713cf8c0670938a9da80a7ca00ea2269f86900f
A714\1dam <1garsau@gmail.com>
2025-01-09 21:12:35 +0100

    Sigo bateria

--- src/main/java/org/example/Entrada.java
+++ src/main/java/org/example/Entrada.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Entrada {
@@ -7,16 +8,33 @@ public class Entrada {
     static Scanner entrada = new Scanner(System.in);
 
     public static int entradaInt(){
-        String numero = entrada.next();
-            
-        try{
-
+        int numero=0;
+        boolean error=true;
+        while(error) {
+            try {
+                String cadena = entrada.next();
+                numero = Integer.parseInt(cadena);
+                error=false;
+            } catch (NumberFormatException e) {
+                System.out.println("ERROR. Entrada no válida, vuelve a introducir el número:");
+                entrada.nextLine();
+            }
         }
+        return numero;
     }
 
     public static String entradaString(){
-
+        String cadena = "";
+        boolean error =true;
+        while (error){
+            cadena= entrada.next();
+            if(!cadena.equals("M") || !cadena.equals("X")){
+                System.out.println("ERROR. Entrada no válida, introduce una M o una X");
+            }else {
+                error=false;
+            }
+        }
+        return cadena;
     }
-
 
 }
