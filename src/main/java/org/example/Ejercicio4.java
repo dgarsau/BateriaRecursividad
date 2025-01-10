b7a98e6714fca6668cb1c13f0818c3df38748983
A714\1dam <1garsau@gmail.com>
2025-01-10 16:32:11 +0100

    sigo recursividad

--- src/main/java/org/example/Ejercicio4.java
+++ src/main/java/org/example/Ejercicio4.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.sql.SQLOutput;
-
 public class Ejercicio4 {
     public static void ejercicio4(){
         System.out.println("Ejercicio 4. Comprobar si un número es binario.");
